223ff5f69914196118550da9a2507c229aeae81c
seonggul <gul930429@gmail.com>
2020-08-24 19:26:37 +0900

    Updates

---
+++ precache-manifest.ecdd1759404fbde241b05b5a9a1b3606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2c4f980483442cf8f4ae500bd1b67e",
+    "url": "/seonggul/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ff4c3079281ccf26e1a4",
+    "url": "/seonggul/movie_app_2020/static/css/main.46f00f78.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/seonggul/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seonggul/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4c3079281ccf26e1a4",
+    "url": "/seonggul/movie_app_2020/static/js/main.34ef481f.chunk.js"
+  },
+  {
+    "revision": "acbc4959b59bd314c2ed",
+    "url": "/seonggul/movie_app_2020/static/js/runtime-main.d3c4cbbd.js"
+  }
+]);
